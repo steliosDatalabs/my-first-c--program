3a6921a2e4d5e3c86b3036c99c2244c99d355787
steliosDatalabs <118010887+steliosDatalabs@users.noreply.github.com>
2022-12-16 20:45:06 +0200

    Update main.cs
    A much needed addition

--- main.cs
+++ main.cs
@@ -10,7 +10,7 @@ namespace CSharpTutorials
     {
         static void Main(string[] args)
         {
-            string message = "Hello World!!";
+            string message = "Hello World!! This is me!";
 
             Console.WriteLine(message);
         }
